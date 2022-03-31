7234f53b04f1c90a256993d74840a21441c4ee34
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2022-03-31 14:35:35 -0400

    Update tos3.py

--- CatalogBuilder/tos3.py
+++ CatalogBuilder/tos3.py
@@ -4,7 +4,7 @@ BUCKET_NAME = "cmip6-nc"
 f_in = open(catalog)
 catalog_name_gz = "esgf-world.csv.gz"
 
-f_out = gzip.open(catalog_name_gz', 'wb')
+f_out = gzip.open(catalog_name_gz, 'wb')
 f_out.writelines(f_in)
 f_out.close()
 f_in.close()
