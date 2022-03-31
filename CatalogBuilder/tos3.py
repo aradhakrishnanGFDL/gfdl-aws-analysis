648956f632ea075d913de9f648194a3f5adfcf4c
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2022-03-30 22:31:09 -0400

    Create tos3.py

---
+++ CatalogBuilder/tos3.py
@@ -0,0 +1,16 @@
+import gzip
+catalog = 'esgf-world.csv'
+BUCKET_NAME = "cmip6-nc"
+f_in = open(catalog)
+catalog_name_gz = "esgf-world.csv.gz"
+
+f_out = gzip.open(catalog_name_gz', 'wb')
+f_out.writelines(f_in)
+f_out.close()
+f_in.close()
+
+
+s3_path = f"{BUCKET_NAME}/{catalog_name_gz}"
+
+try:
+s3.put(catalog_name_gz, s3_path)
